4f932e1aef3f6775ced934d8f0e5ec81abeb4e3c
Eunhye Kim <eunhyekim1223@gmail.com>
2022-03-16 16:34:07 +0900

    Education 스키마 추가 및 Education MVP 생성 API 구현

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Certificate } from "./models/Certificate";
 import { Award } from "./models/Award"
+import { Education } from "./models/Education";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -17,4 +18,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Certificate, Award };
+export { User, Certificate, Award, Education };
