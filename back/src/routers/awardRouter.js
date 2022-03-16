fe105a7a5a59291d100403995c84b5b10191c85d
nohnoori <noori105066@gmail.com>
2022-03-16 18:43:02 +0900

    middleware 위치 수정

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -36,45 +36,39 @@ awardAuthRouter.post("/award/create", async function (req, res, next) {
   }
 });
 
-awardAuthRouter.get(
-  "/awards/:id",
-  login_required,
-  async function (req, res, next) {
-    try {
-      const id = req.params.id
-      const currentAwardInfo = await awardService.getAward({
-        id,
-      });
+awardAuthRouter.get("/awards/:id", async function (req, res, next) {
+  try {
+    const id = req.params.id
+    const currentAwardInfo = await awardService.getAward({
+      id,
+    });
 
-      if (currentAwardInfo.errorMessage) {
-        throw new Error(currentAwardInfo.errorMessage);
-      }
-
-      res.status(200).json(currentAwardInfo);
-    } catch (error) {
-      next(error);
+    if (currentAwardInfo.errorMessage) {
+      throw new Error(currentAwardInfo.errorMessage);
     }
+
+    res.status(200).json(currentAwardInfo);
+  } catch (error) {
+    next(error);
   }
+}
 );
 
-awardAuthRouter.get(
-  "/awardlist/:user_id",
-  login_required,
-  async function (req, res, next) {
-    try {
-      const user_id = req.params.user_id
-      // 사용자의 수상 목록을 얻음
-      const awards = await awardService.getAwards({ user_id });
-      res.status(200).json(awards);
-    } catch (error) {
-      next(error);
-    }
+awardAuthRouter.get("/awardlist/:user_id", async function (req, res, next) {
+  try {
+    const user_id = req.params.user_id
+    // 사용자의 수상 목록을 얻음
+    const awards = await awardService.getAwards({ user_id });
+    res.status(200).json(awards);
+  } catch (error) {
+    next(error);
   }
+}
 );
 
 
 // jwt 토큰 기능 확인용, 삭제해도 되는 라우터임.
-awardAuthRouter.get("/afterlogin", login_required, function (req, res, next) {
+awardAuthRouter.get("/afterlogin", function (req, res, next) {
   res
     .status(200)
     .send(
