c12850ca6662c8c496aed8814326e5ebc8220a9f
nuri <noori1050@naver.com>
2022-03-19 21:14:46 +0900

    [Modify] Eslint 설정

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,31 +1,32 @@
-import React,{ useState } from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 import AwardCard from "./AwardCard";
 import AwardEditForm from "./AwardEditForm";
 
 // - 각 Award 컴포넌트는 isEditing 상태에 따라, false면 AwardCard, true면 AwardEditForm이 됩니다.
 // - isEditable(포트폴리오 소유자와 현재 로그인한 사용자가 일치할 때)이 true인 경우 편집 버튼이 생깁니다.
 
-function Award({isEditable,portfolioOwnerId,currentAward,setAward}) {
-  const [ isEditing , setIsEditing ] = useState(false);
+function Award({ isEditable, portfolioOwnerId, currentAward, setAward }) {
+  const [isEditing, setIsEditing] = useState(false);
 
-  return(
+  return (
     <>
-      {isEditing ?(
+      {isEditing ? (
         <AwardEditForm
-          setIsEditing = {setIsEditing}
+          setIsEditing={setIsEditing}
           setAward={setAward}
-          currentAward = {currentAward}
-        />  
+          currentAward={currentAward}
+        />
       ) : (
         <AwardCard
-          setIsEditing = {setIsEditing}
-          isEditable = {isEditable}
+          setIsEditing={setIsEditing}
+          isEditable={isEditable}
           award={currentAward}
-          portfolioOwnerId = {portfolioOwnerId}
+          portfolioOwnerId={portfolioOwnerId}
         />
       )}
     </>
-  )
-};
+  );
+}
 
 export default Award;
--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -1,18 +1,19 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { Button, Form, Card, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
 
-function AwardAddForm ({setIsAdding,setAward, portfolioOwnerId}) {
+function AwardAddForm({ setIsAdding, setAward, portfolioOwnerId }) {
   // 수상 내역
   const [title, setTitle] = useState("");
   // 상세 내역
   const [description, setDescription] = useState("");
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     // user_id는 현재 사용자의 portfolioOwnerId.
     const user_id = portfolioOwnerId;
-    
+
     // "award/create" 엔드포인트로 post요청함.
     await Api.post("award/create", {
       user_id,
@@ -22,39 +23,42 @@ function AwardAddForm ({setIsAdding,setAward, portfolioOwnerId}) {
 
     // 유저 id를 가지고 "awardlist/유저id" 엔드포인트로 요청해 사용자 정보를 불러옴.
     // 가져온 사용자 정보를 award에 넣음.
-    await Api.get("awardlist",user_id)
-             .then((res) => setAward(res.data));
+    await Api.get("awardlist", user_id).then((res) => setAward(res.data));
 
-  // award 편집 끝!
-  setIsAdding(false);
-  }
-  return(
-    <Form onSubmit = {handleSubmit}>
-      <Form.Group controlId = "awardEditTitle" className="mb-2">
-        <Form.Control 
+    // award 편집 끝!
+    setIsAdding(false);
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="awardEditTitle" className="mb-2">
+        <Form.Control
           className="mb-2"
-          type = "title" 
-          placeholder = "수상 내역" 
-          value = {title||''}
-          onChange = {(e) => setTitle(e.target.value)}
-           />
-        <Form.Control 
+          type="title"
+          placeholder="수상 내역"
+          value={title || ""}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Form.Control
           className="mb-2"
-          type = "description" 
-          placeholder = "상세 내역" 
-          value = {description||''}
-          onChange = {(e) => setDescription(e.target.value)}
-          />
+          type="description"
+          placeholder="상세 내역"
+          value={description || ""}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group as={Row} className="mt-3 text-center">
         <Col sm={{ span: 20 }}>
-          <Button variant="primary" type="submit" className="me-2" >확인</Button>
-          <Button variant="secondary" onClick={() => setIsAdding(false)}>취소</Button>
+          <Button variant="primary" type="submit" className="me-2">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsAdding(false)}>
+            취소
+          </Button>
         </Col>
       </Form.Group>
     </Form>
-  )
+  );
 }
 
 export default AwardAddForm;
