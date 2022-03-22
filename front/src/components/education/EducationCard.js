9429b8ad828b98ad873d498944399a586946468e
KimByeolhee <qufgml0216@naver.com>
2022-03-22 14:58:50 +0900

    [Add]: Eduaction 삭제 버튼 추가

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,5 +1,6 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Button, Row, Col, Modal } from "react-bootstrap";
 import { useState } from "react";
+import * as Api from "../../api";
 // 하위 컴포넌트
 import EducationEditForm from "./EducationEditForm";
 
@@ -8,6 +9,13 @@ import EducationEditForm from "./EducationEditForm";
 
 function EducationCard({ education, isEditable }) {
   const [isEditing, setIsEditing] = useState(false); // 편집 폼이 보이는지 여부
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = async () => {
+    await Api.delete(`educations/${education.id}`);
+  };
 
   return (
     <Card.Text>
@@ -29,16 +37,53 @@ function EducationCard({ education, isEditable }) {
         )}
 
         {isEditable && (
-          <Col xs lg="1">
+          <Col xs lg="2">
             {!isEditing && ( // 편집 버튼을 누르면 isEditing이 true가 되면서 편집 버튼이 사라지고 폼만 보임
-              <Button
-                variant="outline-info"
-                size="sm"
-                onClick={() => setIsEditing((prev) => !prev)}
-                className="mr-3"
-              >
-                편집
-              </Button>
+              <>
+                <Button
+                  variant="outline-info"
+                  size="sm"
+                  onClick={() => setIsEditing((prev) => !prev)}
+                  className="mr-3"
+                >
+                  편집
+                </Button>{" "}
+                <Button
+                  variant="outline-danger"
+                  size="sm"
+                  onClick={() => handleShow()}
+                  className="mr-3"
+                >
+                  삭제
+                </Button>
+                <Modal
+                  show={show}
+                  onHide={handleClose}
+                  backdrop="static"
+                  keyboard={false}
+                >
+                  <Modal.Header closeButton>
+                    <strong>정말 삭제하시겠습니까?</strong>
+                  </Modal.Header>
+                  <Modal.Body>
+                    삭제를 하시는 경우 복구가 불가능합니다.
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        handleDelete();
+                        handleClose();
+                      }}
+                    >
+                      삭제
+                    </Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                      취소
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
             )}
           </Col>
         )}
